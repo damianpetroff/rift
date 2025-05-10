0f6989bf9e803cea80b95e42e9d12e033a623418
Damian Petroff <damian.petroff@outlook.com>
2025-05-10 21:12:04 +0200

    ui: grid

--- src/app/_components/ChampionCard.tsx
+++ src/app/_components/ChampionCard.tsx
@@ -5,21 +5,41 @@ export default function ChampionCard({
   champion,
   patch,
 }: Readonly<{ champion: ChampionDetailed; patch: string }>) {
-  const splashImage = getImageUrl({ type: 'loading', patch, imageFile: `${champion.id}_0.jpg` });
-  if (champion.id.startsWith('Aurel')) console.log(champion);
+  const img = getImageUrl({ type: 'loading', patch, imageFile: `${champion.id}_3.jpg` });
   return (
-    <div className="relative h-72 w-40" key={champion.key}>
+    <div
+      className="relative z-10 h-72 w-40 overflow-hidden rounded-sm border border-white/25 transition-transform duration-100 ease-in-out hover:z-20 hover:scale-105"
+      key={champion.key}
+    >
       <Image
-        src={splashImage}
+        src={img}
         alt={champion.name}
         style={{ objectFit: 'cover' }}
         fill
         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
       />
 
-      <h2 className="bg-opacity-50 absolute right-0 bottom-0 left-0 p-2 text-center text-white">
-        {champion.name}
-      </h2>
+      <div className="absolute right-0 bottom-0 left-0 flex flex-col gap-y-2 p-2">
+        <h2 className="text-center text-xs font-bold tracking-widest text-white/80 uppercase">
+          {champion.name}
+        </h2>
+        <div className="flex justify-center gap-1">
+          {champion.spells.map(spell => (
+            <span
+              key={spell.id}
+              className="relative inline-block size-6 overflow-hidden rounded border border-black/10 shadow-sm shadow-black"
+            >
+              <Image
+                src={getImageUrl({ type: 'spell', patch, imageFile: spell.image.full })}
+                alt={spell.name}
+                style={{ objectFit: 'cover' }}
+                fill
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              />
+            </span>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,15 @@ export default async function HomePage() {
   const patch = await getLatestPatch();
   const championsId = (await getChampions({ patch })).map(champ => champ.id);
   const champions = await Promise.all(championsId.map(id => getChampion({ patch, champion: id })));
-  console.log(champions[0]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p className="text-xs">Latest Patch: {patch}</p>
-      <div className="z-10 flex w-full max-w-7xl flex-col items-center justify-between gap-4 font-mono text-sm lg:flex-row">
+      <div className="z-10 flex w-full max-w-7xl flex-col items-center justify-between gap-4 text-sm lg:flex-row">
         <div className="flex w-full flex-col items-center justify-between gap-12">
-          <h1 className="text-center text-2xl font-bold text-balance">Champions</h1>
+          <h1 className="my-4 text-center text-4xl font-bold text-balance">Champions</h1>
 
-          <div className="grid grid-cols-6 gap-4 font-normal">
+          <div className="grid grid-cols-2 gap-4 font-normal sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
             {champions.map(champion => (
               <ChampionCard key={champion.id} champion={champion} patch={patch} />
             ))}
