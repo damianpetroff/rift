75731959e2b1f3fe00241093fa27b99f3f1fdd04
Damian Petroff <damian.petroff@outlook.com>
2025-05-10 21:31:11 +0200

    seo: change title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
-import type { Metadata } from "next";
-import { Open_Sans } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Open_Sans } from 'next/font/google';
+import './globals.css';
 
-const inter = Open_Sans({ subsets: ["latin"] });
+const inter = Open_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Random Recipe",
-  description: "Random recipe",
+  title: 'Rift',
+  description: "See the champions of League of Legends, doesn't do anything else yet.",
 };
 
 export default function RootLayout({
